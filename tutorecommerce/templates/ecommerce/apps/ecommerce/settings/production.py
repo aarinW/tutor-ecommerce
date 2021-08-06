c5122fbd4d8eea983a91f00da921808ee29b7563
aarinW <87573158+aarinW@users.noreply.github.com>
2021-08-06 13:58:55 +0530

    Update production.py

--- tutorecommerce/templates/ecommerce/apps/ecommerce/settings/production.py
+++ tutorecommerce/templates/ecommerce/apps/ecommerce/settings/production.py
@@ -4,4 +4,4 @@ from ..production import *
 
 SOCIAL_AUTH_EDX_OAUTH2_PUBLIC_URL_ROOT = "{% if ENABLE_HTTPS %}https{% else %}http{% endif %}://{{ LMS_HOST }}"
 
-BACKEND_SERVICE_EDX_OAUTH2_KEY = "{{ ECOMMERCE_OAUTH2_KEY }}
+BACKEND_SERVICE_EDX_OAUTH2_KEY = "{{ ECOMMERCE_OAUTH2_KEY }}"
